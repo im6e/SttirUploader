35dcd0a54cbd519ee46d13cb2fe3975c83f9f720
Takuo-IMBE <imbe@amoad.com>
2014-01-27 23:05:06 +0900

    deleted unnecessary code and added comments

--- SttirUploader/ProjectUploader.m
+++ SttirUploader/ProjectUploader.m
@@ -32,66 +32,10 @@
 
 
     // -------------------- upload file --------------------
-    NSString *file = [NSString stringWithFormat:@"file://%@", [fileList objectAtIndex:0]];
-    NSLog(@"%@", file);
-//    NSMutableURLRequest *request = [[AFHTTPRequestSerializer serializer] multipartFormRequestWithMethod:@"POST"
-//                                                                                              URLString:@"http://172.19.201.32:3000/upload"
-//                                                                                             parameters:nil
-//                                                                              constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-//                                        [formData appendPartWithFileURL:[NSURL fileURLWithPath:file]
-//                                                                   name:@"file"
-//                                                               fileName:@"filename.txt"
-//                                                               mimeType:@"text/plain"
-//                                                                  error:nil
-//                                         ];
-//    } error:nil];
-//
-//    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-//    NSProgress *progress = nil;
-//
-//    NSURLSessionUploadTask *uploadTask = [manager uploadTaskWithStreamedRequest:request
-//                                                                       progress:&progress
-//                                                              completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
-//                                                                  if (error) {
-//                                                                      NSLog(@"Error: %@", error);
-//                                                                  } else {
-//                                                                      NSLog(@"%@ %@", response, responseObject);
-//                                                                  }
-//                                                              }
-//                                          ];
-//
-//    [uploadTask resume];
-
-//    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-//    NSDictionary *parameters = @{@"foo": @"bar"};
-//    NSURL *filePath = [NSURL fileURLWithPath:file];
-//    NSData *data = [[NSFileManager defaultManager] contentsAtPath:file];
-//    [manager POST:@"http://172.19.201.32:3000/upload" parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData> formData)
-//     {
-//         [formData appendPartWithFileURL:filePath
-//                                    name:@"file"
-//                                fileName:@"filename.jpg"
-//                                mimeType:@"image/jpeg"
-//                                   error:nil
-//          ];
-//     }
-//          success:^(AFHTTPRequestOperation *operation, id responseObject)
-//     {
-//         NSLog(@"Success: %@", responseObject);
-//     }
-//          failure:^(AFHTTPRequestOperation *operation, NSError *error)
-//     {
-//         NSLog(@"Error: %@", error);
-//         NSLog(@"all headers: %@", [[operation.response allHeaderFields] descriptionInStringsFileFormat]);
-//         NSLog(@"request: %@", [[operation.request allHTTPHeaderFields] descriptionInStringsFileFormat]);
-//         NSString *req = [[NSString alloc] initWithData:operation.request.HTTPBody encoding:NSUTF8StringEncoding];
-//         NSLog(@"req: %@", req);
-//     }
-//     ];
-
-
+    // TODO: 冗長なので別クラスにする
+    // TODO: async 使う？
+    // TODO: AFNetworking 使えないか？
     NSData *data = [[NSFileManager defaultManager] contentsAtPath:[fileList objectAtIndex:0]];
-    NSLog(@"data: %@", data);
 
     //ここからPOSTDATAの作成
     NSString *urlString = @"http://172.19.201.32:3000/upload";
